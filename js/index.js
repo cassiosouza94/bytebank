d717a4d7b35c5df4c4ed60e167e68bda38bf9752
cassioasouzaa <cassioasouzaa@gmail.com>
2023-03-07 22:24:09 -0300

    Prosseguindo com o projeto

--- js/index.js
+++ js/index.js
@@ -7,4 +7,4 @@ const cliente2 = new Cliente('Marinalva Oliveira', '30.970.549-6', '378345950/83
 const contaCorrenteCliente1 = new ContaCorrente('0001', cliente1, '4363533-9', 'Banco 0260.Nu Pagamentos S.A.');
 const contaCorrenteCliente2 = new ContaCorrente('4157', cliente2, '220901400045677-9', 'Caixa Econômica Federal');
 
-console.log(ContaCorrente.numeroDeContas);
+console.log(ContaCorrente.numeroDeContas); 
